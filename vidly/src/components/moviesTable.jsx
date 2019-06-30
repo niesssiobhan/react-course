4ec2e68d54ed3e331fc6125ce65ecf65144e57ee
niesssiobhan <niesssiobhan@live.com>
2019-06-30 14:24:27 -0700

    saving point

---
+++ vidly/src/components/moviesTable.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import TableHeader from "./common/tableHeader";
+import TableBody from "./common/tableBody";
+import Like from "./common/like";
+
+class MoviesTable extends Component {
+  columns = [
+    { path: "title", label: "Title" },
+    { path: "genra.name", label: "Genre" },
+    { path: "numberInStock", label: "Stock" },
+    { path: "dailyRentalRate", label: "Rate" },
+    {
+      key: "like",
+      content: movie => (
+        <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} />
+      )
+    },
+    {
+      key: "delete",
+      content: movie => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-sm"
+        >
+          Delete
+        </button>
+      )
+    }
+  ];
+
+  render() {
+    const { movies, onSort, sortColumn } = this.props;
+
+    return (
+      <table className="table">
+        <TableHeader
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+        />
+        <TableBody columns={this.columns} data={movies} />
+      </table>
+    );
+  }
+}
+
+export default MoviesTable;
+
+//-----------Notes---------//
+
+// content is a function that takes movie or an items and returns a React Element (<Like .....)
